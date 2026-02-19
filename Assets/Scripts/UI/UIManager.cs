5f7ef12f9155b8454af8f442a2434de1ea2226f8
Thitirat12 <thitirat.phua@bumail.net>
2026-02-19 19:35:05 +0700

    Add some Ui
    w5 work but not finish

---
+++ Assets/Scripts/UI/UIManager.cs
@@ -0,0 +1,16 @@
+using NUnit.Framework;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform selectionBox;
+    public RectTransform SelectionBox { get { return selectionBox; } }
+
+    public static UIManager instance;
+
+    private void Awake()
+    {
+        instance = this;    
+    }
+}
